e0b6b6da28718a3ab21301879173e7799afc5230
Chen Jiang <chen.jiang.1206@gmail.com>
2023-03-30 17:40:32 -0400

    change a few this but not understanding why

--- index.js
+++ index.js
@@ -9,6 +9,68 @@
  for you to use if you need it!
  */
 
+
+//PASSING EMPLOYEERECORD NOT AS A PARAMETER BUT CREATE IT AS A CONTEXT 
+
+function createEmployeeRecord(array) {
+    const employeeRecord = {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents:[],
+        timeOutEvents: [], 
+    }
+
+    return employeeRecord
+}
+
+function createEmployeeRecords(array) {
+    return array.map(createEmployeeRecord)
+}
+
+function createTimeInEvent(timeInStamp){
+    const newTimeStamp = {
+        type: "TimeIn",
+        hour: Number(timeInStamp.slice(-4)),
+        date: timeInStamp.slice(0, -5)
+    }
+    this.timeInEvents.push(newTimeStamp) 
+    return this
+}
+
+function createTimeInEvent(timeInStamp){
+    const newTimeStamp = {
+        type: "TimeIn",
+        hour: Number(timeInStamp.slice(-4)),
+        date: timeInStamp.slice(0, -5)
+    }
+    this.timeInEvents.push(newTimeStamp) 
+    return this
+}
+
+function createTimeOutEvent(timeOutStamp){
+    const newTimeStamp = {
+        type: "TimeOut",
+        hour: Number(timeOutStamp.slice(-4)),
+        date: timeOutStamp.slice(0, -5)
+    }
+    this.timeOutEvents.push(newTimeStamp) 
+    return this
+}
+
+function hoursWorkedOnDate(date) {
+    const timeInObj = this.timeInEvents.find(ele => ele.date === date)
+    const timeOutObj = this.timeOutEvents.find(ele => ele.date === date)
+    return Math.floor((timeOutObj.hour - timeInObj.hour)/100)
+}
+
+function wagesEarnedOnDate(date) {
+    const hours = hoursWorkedOnDate(date)
+    const rate = this.payPerHour
+    return hours * rate
+}
+
 const allWagesFor = function () {
     const eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
@@ -19,5 +81,11 @@ const allWagesFor = function () {
     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
     return payable
+}
+
+function calculatePayroll(employeesArray) {
+    const wagesArr = employeesArray.map(allWagesFor)
+    const payRoll = wagesArr.reduce((total, currentWage) => total += currentWage, 0)
+    return payRoll
 }
 
